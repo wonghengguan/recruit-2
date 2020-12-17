b34ee23a1a776d7f0ce5661979c3acd8342c90b0
Henry Wong <henry.wong@pymsoft.com.my>
2020-12-17 17:15:44 +0800

    - Update project notes - Added SQL script

---
+++ Test script.sql
@@ -0,0 +1,13 @@
+insert into users
+values('test','test')
+insert into users
+values('admin','admin')
+
+insert into cars
+values('Mazda',9.5,1,'Mazda 3',1399,2020,'Sedan',5,'BCD234')
+insert into cars
+values('Mazda',8.5,1,'Mazda 2',699,2018,'Sedan',5,'ABC123')
+insert into cars
+values('Honda',8.8,1,'Honda City',749,2020,'Sedan',5,'CDE567')
+insert into cars
+values('Mini',9.9,1,'Mini cooper',1799,2019,'Sedan',3,'AAA1')
